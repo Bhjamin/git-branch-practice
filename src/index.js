29515dadbd7e234c46eeb52e49f31d6a79111e70
Bhjamin <benjamin@hancock1.com>
2023-04-04 10:03:13 -0600

    added routes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App>
+        <Routes>
+          <Route/>
+          <Route/>
+        </Routes>
+      </App>
     </BrowserRouter>
   </React.StrictMode>
 );
